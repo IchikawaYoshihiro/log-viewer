11a74d35e4d397b4eb5da990aef18b5139f3711e
yosichikaw <yosichikaw@pasonacareer.biz>
2018-05-02 18:07:40 +0900

    fix indent

--- src/Controllers/LogViewerController.php
+++ src/Controllers/LogViewerController.php
@@ -14,83 +14,83 @@ class LogViewerController extends BaseController
     }
 
     public function index()
-	{
-		$files = static::getLogFileList();
-		return view('logviewer::index', compact('files'));
-	}
+    {
+        $files = static::getLogFileList();
+        return view('logviewer::index', compact('files'));
+    }
 
 
-	public function show(Request $request, $filename)
-	{
-		$path = static::getLogFilePath($filename);
+    public function show(Request $request, $filename)
+    {
+        $path = static::getLogFilePath($filename);
 
-		if (!$path) {
-			return redirect()->route('logviewer::index');
-		}
+        if (!$path) {
+            return redirect()->route('logviewer::index');
+        }
 
-		$logs = static::analyzeLogFile($path);
-		return view('logviewer::show', compact('logs', 'filename'));
-	}
-
-
-	/**
-	 * get list of the log file name
-	 * @return array filename list
-	 */
-	private static function getLogFileList()
-	{
-		return array_map('basename', glob(storage_path('logs/*.log')));
-	}
+        $logs = static::analyzeLogFile($path);
+        return view('logviewer::show', compact('logs', 'filename'));
+    }
 
 
-	/**
-	 * get log file path from filename
-	 * @param string $filename
-	 * @return string|null full file path
-	 */
-	private static function getLogFilePath($filename)
-	{
-		$file = array_map('realpath', glob(storage_path("logs/{$filename}")));
-		return array_shift($file);
-	}
+    /**
+     * get list of the log file name
+     * @return array filename list
+     */
+    private static function getLogFileList()
+    {
+        return array_map('basename', glob(storage_path('logs/*.log')));
+    }
 
 
-	/**
-	 * explode log contents
-	 * @param string $path path to log file
-	 * @return array
-	 */
-	private static function analyzeLogFile($path)
-	{
-		$handle = fopen($path, 'r');
-
-		$contents = [];
-		$tmp = '';
-
-		do {
-			$current = fgets($handle);
-			if (static::isNextLine($current)) {
-				// 空行スキップ
-				if($tmp) {
-					$contents[] = Log::create($tmp);
-				}
-				$tmp = $current;
-			}
-			else {
-				$tmp .= $current;
-			}
-		} while ($handle && !feof($handle));
-
-		return $contents;
-	}
+    /**
+     * get log file path from filename
+     * @param string $filename
+     * @return string|null full file path
+     */
+    private static function getLogFilePath($filename)
+    {
+        $file = array_map('realpath', glob(storage_path("logs/{$filename}")));
+        return array_shift($file);
+    }
 
 
-	/**
-	 * text contain datetime string
-	 * @return bool
-	 */
-	private static function isNextLine($str)
-	{
-		return preg_match('/^\[\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}\]/', $str);
-	}
+    /**
+     * explode log contents
+     * @param string $path path to log file
+     * @return array
+     */
+    private static function analyzeLogFile($path)
+    {
+        $handle = fopen($path, 'r');
+
+        $contents = [];
+        $tmp = '';
+
+        do {
+            $current = fgets($handle);
+            if (static::isNextLine($current)) {
+                // 空行スキップ
+                if($tmp) {
+                    $contents[] = Log::create($tmp);
+                }
+                $tmp = $current;
+            }
+            else {
+                $tmp .= $current;
+            }
+        } while ($handle && !feof($handle));
+
+        return $contents;
+    }
+
+
+    /**
+     * text contain datetime string
+     * @return bool
+     */
+    private static function isNextLine($str)
+    {
+        return preg_match('/^\[\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}\]/', $str);
+    }
 }
--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -3,11 +3,11 @@
 @section('title', __('logviewer::message.top'))
 
 @section('content')
-	<ul>
-	@forelse ($files as $file)
-		<li><a href="{{ route('logviewer::show', $file) }}">{{ $file }}</a></li>
-	@empty
-		<li>{{ __('logviewer::message.no_files') }}</li>
-	@endforelse
-	</ul>
+    <ul>
+    @forelse ($files as $file)
+        <li><a href="{{ route('logviewer::show', $file) }}">{{ $file }}</a></li>
+    @empty
+        <li>{{ __('logviewer::message.no_files') }}</li>
+    @endforelse
+    </ul>
 @endsection
